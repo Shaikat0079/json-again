49a00e603e8b5fefe288a163658b2acb0bb84494
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-16 05:30:02 +0930

    Loaded all songs

--- script.js
+++ script.js
@@ -13,10 +13,52 @@ function displayCategories(categories){
         categoryDiv.innerHTML = 
         ` 
 
-        <button class="btn btn-sm">${cat.category}</button>
+        <button class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>
         
         `
         categoryContainer.append(categoryDiv)
     }
 }
 loadCategories()
+
+function loadVideos(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res=>res.json())
+    .then(data=>displayVideos(data.videos))
+}
+
+loadVideos()
+
+const displayVideos = (videos)=>{
+    const videoContainer = document.getElementById('video-container');
+    videos.forEach(video => {
+        // console.log(video)
+        const videoCard = document.createElement("div");
+
+        videoCard.innerHTML = `
+        
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      ${video.title}
+      ${
+        video.authors[0].verified===true ? `<div class="badge badge-secondary">Verified</div>`: ""
+      }
+    </h2>
+    <p>${video.description}</p>
+    <div class="card-actions justify-end">
+      <div class="badge badge-outline">Fashion</div>
+      <div class="badge badge-outline">Products</div>
+    </div>
+  </div>
+</div>
+        `
+        videoContainer.append(videoCard)
+    });
+
+}
