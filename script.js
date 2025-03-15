c133a732e812cfffdbafacfcfb5da48d3cd22dee
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-16 02:47:50 +0930

    Categories added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function loadCategories(){
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then(res=>res.json())
-    .then(data=>console.log(data.categories));
+    .then(data=>displayCategories(data.categories));
 }
-
+function displayCategories(categories){
+    console.log(categories)
+}
 loadCategories()
